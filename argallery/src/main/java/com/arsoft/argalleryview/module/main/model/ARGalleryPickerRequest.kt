180e7d343b5a7321255b8c7144cf2765fd5327a7
Flyingduck9440 <saharat.cs58@gmail.com>
2023-04-05 13:54:37 +0700

    remove set maximum items in ARGalleryPickerRequest

--- argallery/src/main/java/com/arsoft/argalleryview/module/main/model/ARGalleryPickerRequest.kt
+++ argallery/src/main/java/com/arsoft/argalleryview/module/main/model/ARGalleryPickerRequest.kt
@@ -7,8 +7,7 @@ sealed interface MediaType {
 }
 
 fun ARGalleryPickerRequest(
-    mediaType: MediaType = MediaType.ImageOnly,
-    maxItems: Int = Int.MAX_VALUE
+    mediaType: MediaType = MediaType.ImageOnly
 ) = ARGalleryPickerRequest.Builder()
     .setMediaType(mediaType)
     .build()
@@ -16,26 +15,17 @@ fun ARGalleryPickerRequest(
 class ARGalleryPickerRequest internal constructor() {
     var mediaType: MediaType = MediaType.ImageOnly
         internal set
-//    var maxItems: Int = Int.MAX_VALUE
-//        internal set
 
     class Builder {
         private var mediaType: MediaType = MediaType.ImageOnly
-//        private var maxItems: Int = Int.MAX_VALUE
 
         fun setMediaType(mediaType: MediaType): Builder {
             this.mediaType = mediaType
             return this
         }
 
-//        fun setMaxItems(maxItems: Int): Builder {
-//            this.maxItems = maxItems
-//            return this
-//        }
-
         fun build(): ARGalleryPickerRequest = ARGalleryPickerRequest().apply {
             this.mediaType = this@Builder.mediaType
-//            this.maxItems = this@Builder.maxItems
         }
     }
 }
